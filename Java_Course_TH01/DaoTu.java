6628b34ba61c9040ecdd0c264f1326b9a1baae78
lamcaohuu <38579734+lamcaohuu@users.noreply.github.com>
2018-05-05 15:16:20 +0700

    Add files via upload

---
+++ Java_Course_TH01/DaoTu.java
@@ -0,0 +1,16 @@
+package caohuulam408.gmail.com;
+
+public class DaoTu {
+	public static String thuchien(String s) {
+		String ns = new String();
+		char[] s1 = s.toCharArray();
+		int count = 0;
+		for (int i = 0; i < s1.length; i++){
+			if (s1[i] == ' ' || i == 0){
+				count++;
+			}
+		}
+		System.out.println(count);
+		return ns;
+	}
+}
