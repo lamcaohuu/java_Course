fc4f0ce6e34f53fb7d0e2a4a77987065681cd461
lamcaohuu <38579734+lamcaohuu@users.noreply.github.com>
2018-05-05 15:11:54 +0700

    Add files via upload

---
+++ TinhTong.java
@@ -0,0 +1,11 @@
+package caohuulam.gmail.com;
+
+public class TinhTong {
+	public static float thuchien(float[] a) {
+		float S = 0;
+		int size = a.length;
+		for (int i = 0; i < size; i++)
+			S += a[i];
+		return S;
+	}
+}
